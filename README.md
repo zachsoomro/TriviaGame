5650bf319cc0b7006c523d079638d6533607a57d
Zach S <zachsoomro@gmail.com>
2019-05-05 15:55:43 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TriviaGame
+
+Timer working
+Score appends to bottom of page cannot figure out how to move into empty upper div
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
   document.getElementById("main").style.visibility = "hidden";
   document.getElementById("score").style.visibility = "hidden";
-
+  //variables for score and the timer
   var correct = 0;
   var incorrect = 0;
   var timesUp = 0;
   var number = 60;
   var intervalId;
-
+  //tells timer to to start on click
   $("#start").on("click", start);
   $("#submit").on("click", stop);
-
+  //1000 = 1 second
   function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
   }
-
+  //alert will appear when timer runs out
   function decrement() {
     number--;
     $("#time").html("Time remaining: " + number);
@@ -24,17 +24,20 @@ $(document).ready(function() {
       alert("Time Up!");
     }
   }
+  //will make questions visible to user
+  //hides intro page
   function start() {
     document.getElementById("main").style.visibility = "visible";
     document.getElementById("intro").style.visibility = "hidden";
     run();
   }
-
+  //hides questions page
+  //displays score page
   function stop() {
     clearInterval(intervalId);
     document.getElementById("score").style.visibility = "visible";
     document.getElementById("main").style.visibility = "hidden";
-
+    //if user input is 'true' value then correct score is increased otherwise incorrect score is increased
     var userInput1 = $("input:radio[name=q1]:checked").val();
     if (userInput1 != undefined) {
       if (userInput1 == "true") {
@@ -45,6 +48,7 @@ $(document).ready(function() {
     } else {
       timesUp++;
     }
+    // var userInput2 == true
 
     var userInput2 = $("input:radio[name=q2]:checked").val();
 
